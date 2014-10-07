78befcbba7d385f370ea41c269a348f99c11213b
Drew Moore <teloaster@yahoo.com>
2014-10-06 23:04:40 -0500

    Full CRUD for Pages in views.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,6 +17,31 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Page at #{@page.controller}##{@page.action} has been updated."
+      redirect_to pages_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    controller = @page.controller
+    action = @page.action
+    if @page.destroy
+      redirect_to pages_path, :notice => "Page at #{controller}##{action} has been deleted."
+    else
+      redirect_to pages_path, :notice => "Page at #{controller}##{action} could not be deleted."
+    end
+  end
+
   private
   def page_params
     params.require(:page).permit(:title, :controller, :action)
